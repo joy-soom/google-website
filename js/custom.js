77125f88ebdaab76a547f5d8e8876ca72ec773fa
park <goslwjddl@naver.com>
2023-05-01 17:28:54 +0900

    disng : footer 반응형 구현

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,24 @@
 $(function () {
-    // scroll js
+  // scroll change download
   $(window).scroll(function () {
     if ($(window).scrollTop() > 500) {
       $(".download").addClass("active");
     } else {
       $(".download").removeClass("active");
     }
+  });
+
+  /* sitemap Accordion */
+  $(".link-item-title").click(function () {
+    // $(this).next().slideToggle()
+    $(this).next().slideDown();
+    $(this).parent().siblings().children(".link-item-content").slideUp();
+    // icon rotate
+    $(this).addClass("active");
+    $(this)
+      .parent()
+      .siblings()
+      .children(".link-item-title")
+      .removeClass("active");
   });
 });
